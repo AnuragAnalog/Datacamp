8ebff834a99d2b01d9dda50f1f6cde614fc5966e
AnuragAnalog <anurag.peddi1998@gmail.com>
2020-02-06 11:26:31 +0530

    reorganized chap1

--- courses/Introduction-to-Network-Analysis-in-Python/Basic-drawing-of-a-network-using-NetworkX.py
+++
@@ -1,8 +0,0 @@
-# Import necessary modules
-import networkx as nx
-from matplotlib import pyplot as plt
-
-# Draw the graph to screen
-nx.draw(T_sub)
-plt.show()
-
--- courses/Introduction-to-Network-Analysis-in-Python/Queries-on-a-graph.py
+++
@@ -1,6 +0,0 @@
-# Use a list comprehension to get the nodes of interest: noi
-noi = [n for n, d in T.nodes(data=True) if d['occupation'] == 'scientist']
-
-# Use a list comprehension to get the edges of interest: eoi
-eoi = [(u, v) for u, v, d in T.edges(data=True) if d['date'] < date(2010,1,1)]
-
