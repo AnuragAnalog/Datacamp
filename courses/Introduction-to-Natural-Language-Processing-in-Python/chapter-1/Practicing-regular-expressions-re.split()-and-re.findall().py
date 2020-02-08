600f8694b9e30bb99dd397259094e315a09d4c62
AnuragAnalog <anurag.peddi1998@gmail.com>
2020-02-09 00:03:50 +0530

    just started the course

---
+++ courses/Introduction-to-Natural-Language-Processing-in-Python/chapter-1/Practicing-regular-expressions-re.split()-and-re.findall().py
@@ -0,0 +1,18 @@
+# Write a pattern to match sentence endings: sentence_endings
+sentence_endings = r"[.?!]"
+
+# Split my_string on sentence endings and print the result
+print(re.split(sentence_endings, my_string))
+
+# Find all capitalized words in my_string and print the result
+capitalized_words = r"[A-Z]\w+"
+print(re.findall(capitalized_words, my_string))
+
+# Split my_string on spaces and print the result
+spaces = r"\s+"
+print(re.split(spaces, my_string))
+
+# Find all digits in my_string and print the result
+digits = r"\d+"
+print(re.findall(digits, my_string))
+
