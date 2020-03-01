12aa898a20bc237f18102e21108412390d8d7ed8
AnuragAnalog <anurag.peddi1998@gmail.com>
2020-03-01 10:16:08 +0530

    added a new course in R

---
+++ courses/R/Introduction-to-R/chapter-1/Arithmetic-with-R.R
@@ -0,0 +1,17 @@
+# An addition
+5 + 5 
+
+# A subtraction
+5 - 5 
+
+# A multiplication
+3 * 5
+
+ # A division
+(5 + 5) / 2 
+
+# Exponentiation
+2 ^ 5
+
+# Modulo
+28 %% 6
---
+++ courses/R/Introduction-to-R/chapter-1/How-it-works.R
@@ -0,0 +1,5 @@
+# Calculate 3 + 4
+3 + 4
+
+# Calculate 6 + 12
+6 + 12
