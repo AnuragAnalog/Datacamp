d043bd2457c093d6ee39b92cc6b3a60092c34665
AnuragAnalog <anurag.peddi1998@gmail.com>
2020-03-01 10:34:55 +0530

    added the other chapters

---
+++ courses/sql/Introduction-to-SQL/chapter-2/Simple-filtering-of-numeric-values.sql
@@ -0,0 +1,8 @@
+-- Instruction1
+select * from films where release_year = 2016;
+
+-- Instruction2
+select count(*) from films where release_year < 2000;
+
+-- Instruction3
+select title, release_year from films where release_year > 2000;
