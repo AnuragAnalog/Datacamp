1a9d5e2eb85aab944c019fc96e32f62f62d5374b
AnuragAnalog <anurag.peddi1998@gmail.com>
2020-04-03 18:35:02 +0530

    added all the scripts

---
+++ courses/python/Intermediate-Importing-Data-in-Python/chapter-3/Plotting-your-Twitter-data.py
@@ -0,0 +1,15 @@
+# Import packages
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+# Set seaborn style
+sns.set(color_codes=True)
+
+# Create a list of labels:cd
+cd = ['clinton', 'trump', 'sanders', 'cruz']
+
+# Plot the bar chart
+ax = sns.barplot(cd, [clinton, trump, sanders, cruz])
+ax.set(ylabel="count")
+plt.show()
+
---
+++ courses/python/Intermediate-Importing-Data-in-Python/chapter-3/Twitter-data-to-DataFrame.py
@@ -0,0 +1,9 @@
+# Import package
+import pandas as pd
+
+# Build DataFrame of tweet texts and languages
+df = pd.DataFrame(tweets_data, columns=['text', 'lang'])
+
+# Print head of DataFrame
+print(df.head())
+
